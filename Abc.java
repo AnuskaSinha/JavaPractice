9f1725ed9aa00b4896874d6400836be1803adc30
AnuskaSinha <anuska.sinha@wissen.com>
2025-09-03 17:05:47 +0530

    first commit

---
+++ Abc.java
@@ -0,0 +1,12 @@
+public class Abc
+{
+	public static void main(String args[])
+	{
+		System.out.println(" ********************************** ");
+		System.out.println("    Welcome to Jenkins ");
+		System.out.println("   From Java file Abc.java ");
+		System.out.println(" Compiling & Executing through Jenkins ");
+		System.out.println("         THANK YOU :)  ");
+		System.out.println(" ********************************** ");
+	}
+}
